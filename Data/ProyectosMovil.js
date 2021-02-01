e859b2ed1a68e54ad768da6ec4b8270b21cc8870
Wilson Castro <wilsoncastro0710@gmail.com>
2021-02-01 17:26:56 -0500

    Movil

---
+++ Data/ProyectosMovil.js
@@ -0,0 +1,47 @@
+export const JsonDataMovil = [
+  {
+    Title: "WhcChat",
+    DescriptionShort: "Mensajería instantanea",
+    Imagen: [{ Image: "/WhcChatUno.jpeg" }],
+    Description: `Aplicación Movil para la comunicación entre personas.`,
+    Tecnologies:
+      "React Native | StyleSheet | Javascript | Typescript | Express | MongoDB | Node.js | Api Rest Full | Expo.io | Socket.io",
+    Links: (
+      <div className="ContainerLinks">
+        <h4>Repositorios GitHub</h4>
+        <div style={{ display: "flex" }}>
+          <div style={{ margin: "5px" }}>
+            <small style={{ display: "block" }}>Aplicación</small>
+            <a
+              style={{ color: "blue" }}
+              href="https://github.com/whccc/ChatWhc"
+              target="_blank"
+            >
+              <img style={{ width: "50px" }} src="/github.png" />
+            </a>
+          </div>
+          <div style={{ margin: "5px" }}>
+            <small style={{ display: "block" }}>Backend-Api</small>
+            <a
+              style={{ color: "blue" }}
+              href="https://github.com/whccc/ChatWhcApi"
+              target="_blank"
+            >
+              <img style={{ width: "50px" }} src="/github.png" />
+            </a>
+          </div>
+          <div style={{ margin: "5px" }}>
+            <small style={{ display: "block" }}>Socket</small>
+            <a
+              style={{ color: "blue" }}
+              href="https://github.com/whccc/ChatWhcSocket"
+              target="_blank"
+            >
+              <img style={{ width: "50px" }} src="/github.png" />
+            </a>
+          </div>
+        </div>
+      </div>
+    ),
+  },
+];
--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import { FcSearch } from "react-icons/fc";
 import { Modal } from "../Modal";
-import { JsonData } from "../../Data/Proyectos";
+import { JsonData } from "../../Data/ProyectosFrontend";
+import { JsonDataMovil } from "../../Data/ProyectosMovil";
 
 export const Projects = () => {
   const [ShowModal, SetShowModal] = useState(false);
   const [IndexData, SetIndexData] = useState(0);
+  const [TypeProject, setTypeProject] = useState("Frontend");
   return (
     <div className="Container">
       <div>
         <h1>Proyectos</h1>
+        <div className="TitleProject">Web</div>
         <div className="ContainerCards">
           {JsonData.map((Data, Index) => {
             return (
@@ -17,6 +20,44 @@ export const Projects = () => {
                 onClick={() => {
                   SetShowModal(!ShowModal);
                   SetIndexData(Index);
+                  setTypeProject("Frontend");
+                }}
+                key={Index}
+              >
+                <h3>
+                  {Data.Title}
+                  <small>{Data.DescriptionShort}</small>
+                </h3>
+                <FcSearch
+                  style={{
+                    fontSize: "40px",
+                    position: "absolute",
+                    right: "10px",
+                    top: "20px",
+                  }}
+                  className="Search"
+                />
+                {Data.Imagen.map((Images, Index) => {
+                  return <img key={Index} src={Images.Image} />;
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <br />
+      <hr />
+      <br />
+      <div>
+        <div className="TitleProject">Movil</div>
+        <div className="ContainerCards">
+          {JsonDataMovil.map((Data, Index) => {
+            return (
+              <div
+                onClick={() => {
+                  SetShowModal(!ShowModal);
+                  SetIndexData(Index);
+                  setTypeProject("Movil");
                 }}
                 key={Index}
               >
@@ -63,6 +104,8 @@ export const Projects = () => {
           display: flex;
           flex-direction: row;
           flex-wrap: wrap;
+          padding-left: 10px;
+          padding-right: 10px;
         }
         .ContainerCards div {
           width: 100%;
@@ -75,17 +118,25 @@ export const Projects = () => {
         .ContainerCards div img {
           width: 100%;
         }
-
+        .TitleProject {
+          font-size: 30px;
+          padding: 10px;
+          text-align: left;
+        }
         @media (min-width: 1024px) {
           .ContainerCards div {
-            max-width: 326.6px;
+            max-width: 319.6px;
           }
         }
       `}</style>
       <Modal
         Show={ShowModal}
         SetShowModal={SetShowModal}
-        Data={JsonData[IndexData]}
+        Data={
+          TypeProject === "Frontend"
+            ? JsonData[IndexData]
+            : JsonDataMovil[IndexData]
+        }
       />
     </div>
   );
